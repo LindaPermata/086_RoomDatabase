d94f858f8795b02e688a0c6d8257b1c2a595941a
LindaPermata <permatalinda531@gmail.com>
2025-11-27 13:40:09 +0700

    Menambahkan EntryViewModel dengan pengelolaan UI state dan fungsi validasi input siswa

---
+++ app/src/main/java/com/example/a086_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.a086_roomdatabase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.a086_roomdatabase.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        })
+    }
+
