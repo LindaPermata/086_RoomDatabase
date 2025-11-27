aeae217c1c87ef8ee2c9c6ec294069a3609b46d6
LindaPermata <permatalinda531@gmail.com>
2025-11-27 13:15:48 +0700

    Menambahkan repositori siswa

---
+++ app/src/main/java/com/example/a086_roomdatabase/repositori/RepositoriSiswa.kt
@@ -0,0 +1,20 @@
+package com.example.a086_roomdatabase.repositori
+
+import com.example.a086_roomdatabase.room.Siswa
+import com.example.a086_roomdatabase.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OffLineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
+}
