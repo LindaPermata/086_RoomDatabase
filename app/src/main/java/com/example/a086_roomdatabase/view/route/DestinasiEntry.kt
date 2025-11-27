1ee7b4e450f16990fab381db786c3e4a0f1e8e0c
LindaPermata <permatalinda531@gmail.com>
2025-11-27 13:32:13 +0700

    menambahkan file destinasientry

---
+++ app/src/main/java/com/example/a086_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,12 @@
+package com.example.a086_roomdatabase.view.route
+
+import com.example.a086_roomdatabase.R
+import com.example.a086_roomdatabase.view.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+
+    override val route = "item_entry"
+
+    override val titleRes = R.string.entry_siswa
+
+}
