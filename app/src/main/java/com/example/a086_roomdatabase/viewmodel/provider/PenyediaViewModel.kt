26b5f7c805bcfbe285f0c2c9b9c5b63511a82dfd
LindaPermata <permatalinda531@gmail.com>
2025-11-27 14:05:12 +0700

    tambah ekstensi CreationExtras untuk akses instance AplikasiSiswa

--- app/src/main/java/com/example/a086_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a086_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,20 +1,24 @@
 package com.example.a086_roomdatabase.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a086_roomdatabase.repositori.AplikasiSiswa
 import com.example.a086_roomdatabase.viewmodel.EntryViewModel
 import com.example.a086_roomdatabase.viewmodel.HomeViewModel
 
-object PenyediaViewModel {
-    val Factory = viewModelFactory {
+object PenyediaViewModel{
+    val Factory = viewModelFactory{
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
-
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
-
     }
 }
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 
