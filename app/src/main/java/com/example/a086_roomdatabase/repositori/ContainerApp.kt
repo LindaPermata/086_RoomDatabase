8fdb3b136abb7bde3256a4ebd564e468a072db0a
LindaPermata <permatalinda531@gmail.com>
2025-11-27 13:21:02 +0700

    Menambahkan container app dan repositori siswa

---
+++ app/src/main/java/com/example/a086_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,22 @@
+package com.example.a086_roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.a086_roomdatabase.room.DatabaseSiswa
+
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
+class AplikasiSiswa : Application() {
+
+}
