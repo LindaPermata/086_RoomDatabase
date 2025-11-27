f3e75daabbc5d047cf6b2da5ba539dff6721cc1c
LindaPermata <permatalinda531@gmail.com>
2025-11-27 13:30:32 +0700

    memperbaiki error

---
+++ app/src/main/java/com/example/a086_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,11 @@
+package com.example.a086_roomdatabase.view.route
+
+import com.example.a086_roomdatabase.R
+import com.example.a086_roomdatabase.view.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+}
