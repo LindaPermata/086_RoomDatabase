e336dccca160b487b294ea7c3863bf22892e159b
LindaPermata <permatalinda531@gmail.com>
2025-12-04 13:49:10 +0700

    tambah code dsn file untuk membuat edit dan delete

--- app/src/main/java/com/example/a086_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a086_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.a086_roomdatabase.repositori.AplikasiSiswa
 import com.example.a086_roomdatabase.viewmodel.DetailViewModel
+import com.example.a086_roomdatabase.viewmodel.EditViewModel
 import com.example.a086_roomdatabase.viewmodel.EntryViewModel
 import com.example.a086_roomdatabase.viewmodel.HomeViewModel
 
@@ -23,6 +24,12 @@ object PenyediaViewModel{
                 this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa
             )
         }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
     }
 }
 
